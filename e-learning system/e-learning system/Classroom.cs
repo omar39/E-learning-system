6ddb7106f10e8fb01d7122c2484c6fb6b3c24c48
AmirAbu-Almajd <amir.abualmajd99@gmail.com>
2020-08-11 22:04:06 +0200

    Student , Subject and Classroom

---
+++ e-learning system/e-learning system/Classroom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_learning_system
+{
+    class Classroom
+    {
+        private String name;
+        readonly int originalCapacity = 30;
+        private Subject subject;
+        private List<Student> students = new List<Student>();
+        // hena el teachers variable
+        public Classroom(String name1 , Subject subj , List<Student> students1)
+        {
+            this.name = name1;
+            this.subject = subj;
+            this.students = students1;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public Subject getSubject()
+        {
+            return this.subject;
+        }
+
+        public int calculateCapacity()
+        {
+            return originalCapacity - students.Count;
+        }
+        public void addStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public void removeStudent(Student student)
+        {
+            students.Remove(student);
+        }
+    }
+}
---
+++ e-learning system/e-learning system/Subject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_learning_system
+{
+    class Subject
+    {
+        private double grade;
+        private String name;
+        private String description;
+        public String getName()
+        {
+            return this.name;
+        }
+        public String getDescription()
+        {
+            return this.description;
+        }
+    }
+}
